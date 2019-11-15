afda796ccafe9548b27ee0af060682e66ccd9010
Brian Burger <bburger@d155.org>
2019-11-15 13:42:21 +0000

    Added Comments for notes

--- Main.java
+++ Main.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
+
 /**
  * Write a description of class StringInputMethods here.
  *
  * @author Mr. Burger
  * @version (a version number or a date)
  */
-public class Main
-{
-    public static void main (String[] args)
-    {
-        String name;
-        int nameLength;
-        String halfName;
+public class Main {
+  public static void main(String[] args) {
+    // Declare variables
+    String name;
+    int nameLength;
+    String halfName;
+    // Need to create a Scanner object to take in inputs from the user
+    Scanner input = new Scanner(System.in);
+    System.out.println("What is your name?");
+    name = input.nextLine();
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is your name?");
-        name=input.nextLine();
+    System.out.println("Hello " + name);
+    // .length() gives the number of characters in the string including spaces and
+    // special characters.
+    nameLength = name.length();
+    System.out.println("Your name has: " + nameLength + " Characters");
+    // Creates a new string that starts at location 0 and ends at but not including
+    // namelength/2
+    halfName = name.substring(0, nameLength / 2);
+    System.out.println(halfName);
+    // Creates a new string that starts at location 3 and goes to the end of the
+    // string
+    halfName = name.substring(3);
+    System.out.println(halfName);
 
-        System.out.println("Hello "+name);
-        nameLength = name.length();
-        System.out.println("Your name has: "+nameLength+" Characters");
+    name = name.replace("a", "z");
+    System.out.println(name);
 
-        halfName = name.substring(0,nameLength/2);
-        System.out.println(halfName);
+    name = name.toUpperCase();
+    System.out.println(name);
 
-        halfName = name.substring(3);
-        System.out.println(halfName);
+    System.out.println("This is a really cool editor.");
 
-        name = name.replace("a","z");
-        System.out.println(name);
-
-        name = name.toUpperCase();
-        System.out.println(name);
-
-        System.out.println("This is a really cool editor.");
-
-
-    }
+  }
 }
